b78e7b7afb369d8477b51b33becd1ad7628d69f3
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-06 19:22:14 +0100

    Bump to 0.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the phpenv cookbook.
 
+## 0.5.1
+
+- [Pierre Rambaud] - Fix typo
+
 ## 0.5.0
 
 - [Pierre Rambaud] - Update dependencies
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pierre.rambaud@numergy.com'
 license 'Apache 2.0'
 description 'Installs/Configures phpenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.5.0'
+version '0.5.1'
 
 depends 'apt'
 depends 'build-essential'
